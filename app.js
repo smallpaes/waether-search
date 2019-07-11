2bd3eac74db613501559f753c0f162ed78e800b0
Mike <mike.huang.mikank@gmail.com>
2019-07-11 11:50:43 +0800

    Finished search function and styling

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
 const express = require('express')
 const exphbs = require('express-handlebars')
+const request = require('request')
+const geocode = require('./geocode')
+const forecast = require('./forecast')
 
 const app = express()
 const port = 3000
@@ -13,6 +16,20 @@ app.use(express.static('public'))
 
 app.get('/', (req, res) => {
   res.render('index')
+})
+
+app.get('/search', (req, res) => {
+  geocode(req.query.address, (error, { longitude, latitude, location } = {}) => {
+    if (error) { return res.send({ error }) }
+    forecast(longitude, latitude, (error, data) => {
+      if (error) { return res.send({ error }) }
+      res.send({
+        forecast: data,
+        location,
+        address: req.query.address
+      })
+    })
+  })
 })
 
 app.listen(port, () => {
---
+++ public/javascripts/app.js
@@ -0,0 +1,29 @@
+const weatherForm = document.querySelector('form')
+const searchInput = document.querySelector('input')
+const infoTitle = document.querySelector('.form-bottom h6')
+const infoContent = document.querySelector('.form-bottom p')
+const formBottom = document.querySelector('.form-bottom')
+weatherForm.addEventListener('submit', (event) => {
+  event.preventDefault()
+  const location = searchInput.value
+
+  fetch(`http://localhost:3000/search?address=${location}`)
+    .then(response => {
+      response.json()
+        .then(data => {
+          formBottom.classList.add('d-block', 'animated', 'fadeInUp')
+          if (data.error) {
+            infoContent.textContent = data.error
+          } else {
+            infoTitle.textContent = data.location
+            infoContent.textContent = data.forecast
+          }
+
+          // listen for animation end
+          formBottom.addEventListener('animationend', () => {
+            formBottom.classList.remove('animated', 'fadeInUp')
+          })
+        })
+    })
+
+})
